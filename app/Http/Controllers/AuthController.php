5441fb12a2cafb17e880b8657cc14527e9c4ce6b
reda-guemry <guemryreda@gmail.com>
2026-03-10 21:49:02 +0000

    add request

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Hash;
 use Illuminate\Http\Request;
@@ -9,13 +10,14 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     
-    public function register(Request $request) 
+    public function register(RegisterRequest $request) 
     {
 
         $user = User::create([
             'name' => $request->name , 
             'email' => $request->email , 
             'password' => Hash::make($request->password) , 
+            'role' => $request->role ?? 'user' ,
         ]); 
 
         $token = $user->createToken('auth_token')->plainTextToken ; 
