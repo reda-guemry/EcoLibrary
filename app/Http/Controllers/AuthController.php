9bdaaa58047e17fc58627984e8a3a4e74435f198
reda-guemry <guemryreda@gmail.com>
2026-03-11 01:29:46 +0000

    add the request of register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,16 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Hash;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    
     public function register(RegisterRequest $request) 
     {
-
         $user = User::create([
             'name' => $request->name , 
             'email' => $request->email , 
@@ -25,10 +24,8 @@ class AuthController extends Controller
         return response()->json([
             'message' => 'User registered successfully' ,
             'token' => $token ,
-            'user'
+            'user' => new UserResource($user) ,
         ]) ;
-
-
     }
 
 
